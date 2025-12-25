eccdde0a8286a8677aef8221d46af79825193dec
abdulmuiz22 <osuolaleabdulmuiz22@gmail.com>
2025-12-25 17:15:21 +0100

    Add portfolio files

---
+++ script.js
@@ -0,0 +1,7 @@
+/* <<<<<<<<<<<<<<<<<<<<<<<<<< typing animation>>>>>>>>>>*/
+var typed = new Typed(".typing", {
+    String:["","web Designer", "Graphics Designer", "Youtuber", "Web Developer"],
+    typeSpeed:150,
+    backSpeed:80,
+    loop:true,
+})
